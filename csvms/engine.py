db37fc02a0e5d9a16100a712ae366178271ba553
Igor C. Formiga <igorformiga125@gmail.com>
2022-07-30 12:42:26 -0300

    #1 Create Table

--- csvms/engine.py
+++ csvms/engine.py
@@ -1,6 +1,8 @@
 """CSVMS SQL Engine Module
 See https://github.com/Didone/csvms/discussions/6
 """
+from mo_sql_parsing import parse
+from csvms.table import Table
 
 class Engine():
     """Class used to implement bootcamp tasks"""
@@ -8,5 +10,30 @@ class Engine():
     def execute(self, sql:str):
         """Execute SQL statement
         :param sql: String with sql statement"""
-        #TODO Implement your SQL engine
-        raise NotImplementedError
+        # TODO Implement your SQL engine
+        ast = parse(sql)
+        
+        # Testando as operações
+        if ast.get('create table') is not None:
+            return self._create_table(
+                tbl_name=ast['create table']['name'],
+                tbl_columns=ast['create table']['columns']
+            )
+        else:
+            raise NotImplementedError
+
+    
+    def _create_table(self, tbl_name:str, tbl_columns:list):
+        # Criação do dicionário que será passado como parâmentro para columns
+        cols = dict()
+        for _c_ in tbl_columns:
+            cnames = _c_['name']
+            # Trasformando a str 'text' no tipo de dado str
+            ctype = Table.dtypes[list(_c_['type'].keys())[0]]
+            cols[cnames] = ctype
+
+        Table(
+            name=tbl_name,
+            columns=cols
+            ).save()
+        return f'A Table {tbl_name} foi criada com sucesso'
