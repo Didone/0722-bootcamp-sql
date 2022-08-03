3218d1fb8242748d24a0fbe3334a3c86ff582490
Fernando Luigi <61151796+fernandodipp@users.noreply.github.com>
2022-08-03 20:20:18 -0300

    #3 Inclusão

--- csvms/engine.py
+++ csvms/engine.py
@@ -17,6 +17,13 @@ class Engine():
                 tbl_name=ast['create table']['name'],
                 tbl_columns=ast['create table']['columns']
             )
+        elif sql.lower().find('insert into') >= 0:
+            if sql.lower().find('commit') >= 0:
+                commitStatus = True
+            sql = sql.replace("\n","")
+            querySplit = sql.split(";")
+            querySplit.pop()
+            return self._insert_table(querySplit,commitStatus)
         else:  
             return None
 
@@ -28,3 +35,24 @@ class Engine():
             cols[cname]=ctype
         Table(name=tbl_name, columns=cols).save()
         return f"A Tabela {tbl_name} foi criada com sucesso!"
+
+    def _insert_table(self, querySplit:list, commitStatus:bool):
+        newValueList = list()
+        for i in range(len(querySplit)):
+            querySplit[i] = querySplit[i].strip()
+            if querySplit[i] != 'COMMIT':    
+                ast = parse(querySplit[i])
+                tbl_name = ast['insert']
+                tbl = Table(tbl_name)
+                newValue = ast['query']['select']
+                for j in range(len(newValue)):
+                    if isinstance(newValue[j]['value'],dict):
+                        newValueList.append(newValue[j]['value']['literal'])
+                    if isinstance(newValue[j]['value'],float):
+                        newValueList.append(newValue[j]['value'])
+        for k in range(0, len(newValueList), 2):
+            tbl.append(newValueList[k],newValueList[k+1])
+        if commitStatus == True:
+            tbl.save()
+        return f"Os valores na tabela {tbl_name} foram inseridos com sucesso"
+    
