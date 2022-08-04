3976c02df745842a74c2cd9b805057b30789f3c7
nascin <mailson.nascin@gmail.com>
2022-08-04 10:08:16 -0300

    #3 Insert Values

--- csvms/engine.py
+++ csvms/engine.py
@@ -2,6 +2,7 @@
 See https://github.com/Didone/csvms/discussions/6
 """
 
+from optparse import Values
 from csvms.table import Table
 from mo_sql_parsing import parse
 
@@ -11,12 +12,39 @@ class Engine():
     def execute(self, sql:str):
         """Execute SQL statement
         :param sql: String with sql statement"""
-        ast = parse(sql)
+        _sql_split = sql.split(';')
+        ast = parse(_sql_split[0])
+
         if ast.get('create table') is not None:
             return self._create_table(
                 tbl_name=ast['create table']['name'],
                 tbl_columns=ast['create table']['columns']
                 )
+        
+        elif ast.get('insert') is not None:
+            ast = sql.split(';')
+            values = list()
+            list_values = list()
+
+            for i in range(0, len(ast)):
+                if 'COMMIT' not in ast[i]:
+                    insert = parse(ast[i])
+                    insert = insert['query']['select']
+                    values = [v['value']['literal'] for v in insert]
+                    list_values.append(values)
+                    values = list()
+                else:
+                    break
+            
+            ast = parse(_sql_split[0])
+            tbl_insert = self._insert_values(
+                tbl_name=ast.get('insert'),
+                insert_values=list_values
+            )
+            ast = sql.split(';')
+            if 'COMMIT' in ast: # Salva no banco se tiver o comando 'COMMIT' 
+                tbl_insert.save()
+
         else:
             raise NotImplementedError
     
@@ -31,3 +59,12 @@ class Engine():
         Table(name=tbl_name, columns=cols).save()
 
         return f'A tabela {tbl_name} foi criada com sucesso!'
+
+    def _insert_values(self, tbl_name:str, insert_values:list):
+        ''' Inserir valores na tabela'''
+        tbl = Table(tbl_name)
+
+        for values in insert_values:
+            tbl.append(*values)
+
+        return tbl
